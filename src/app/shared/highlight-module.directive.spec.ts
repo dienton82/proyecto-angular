b71fb7999b3b6fcc5ff58b773f5df36028252b46
Eduardo Alvarez <yalvarezg82@gmail.com>
2024-10-11 07:39:23 -0500

    Eliminada la carpeta assets

--- src/app/shared/highlight-module.directive.spec.ts
+++ src/app/shared/highlight-module.directive.spec.ts
@@ -1,8 +1,24 @@
 import { HighlightModuleDirective } from './highlight-module.directive';
+import { ElementRef, Renderer2 } from '@angular/core'; // Importa ElementRef y Renderer2
+import 'jasmine';
 
 describe('HighlightModuleDirective', () => {
+  let mockElementRef: ElementRef;
+  let mockRenderer: Renderer2;
+
+  beforeEach(() => {
+    // Crear una instancia simulada de ElementRef
+    mockElementRef = new ElementRef(document.createElement('div'));
+
+    // Simular Renderer2 (podemos crear un objeto vacío porque normalmente no es necesario ejecutar sus métodos en la prueba)
+    mockRenderer = jasmine.createSpyObj('Renderer2', ['setStyle', 'removeStyle']);
+  });
+
   it('should create an instance', () => {
-    const directive = new HighlightModuleDirective();
+    // Pasar tanto ElementRef como Renderer2 al constructor de la directiva
+    const directive = new HighlightModuleDirective(mockElementRef, mockRenderer);
+
+    // Verificar que la directiva se crea correctamente
     expect(directive).toBeTruthy();
   });
 });
