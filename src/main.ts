58ebd667ce77f795559cf9f343460c210bf56b14
Eduardo Alvarez <yalvarezg82@gmail.com>
2025-02-11 19:46:32 -0500

    Actualizado enrutador con hash location para GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes, withHashLocation())]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+  ],
 }).catch(err => console.error(err));
