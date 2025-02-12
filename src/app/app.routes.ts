58a5843c8b416867ee9dad8f62ea12d0d7dd0b1a
yealvarezg <130720872+dienton82@users.noreply.github.com>
2025-02-11 20:42:20 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
-import { Routes } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserFormComponent } from './user/user-form.component';
 
-export const routes: Routes = [
-  { path: 'users', component: UserListComponent },
-  { path: 'users/new', component: UserFormComponent },
-  { path: 'users/:id', component: UserDetailComponent },
-  { path: 'users/edit/:id', component: UserFormComponent },
-  { path: '', redirectTo: '/users', pathMatch: 'full' }
-];
+export const routes = provideRouter(
+  [
+    { path: 'users', component: UserListComponent },
+    { path: 'users/new', component: UserFormComponent },
+    { path: 'users/:id', component: UserDetailComponent },
+    { path: 'users/edit/:id', component: UserFormComponent },
+    { path: '', redirectTo: '/users', pathMatch: 'full' }
+  ],
+  withHashLocation() // Esto activa el modo hash
+);
